a5aea5bba3368124e62dcb89e995ca3beea628ee
Raj <raj_shekar1234@yahoo.co.in>
2013-12-04 00:29:54 +0530

    - area chart commit

--- application/views/user/chart.php
+++ application/views/user/chart.php
@@ -13,7 +13,7 @@
         $(function () {
             $('#container').highcharts({
                 chart: {
-                    type: 'areaspline'
+                    type: 'area'
                 },
                 title: {
                     text: 'IAP Chart on Weight for <strong> BOYS </strong> from 0-36 Months'
@@ -45,15 +45,21 @@
                     }
                 },
                 series: [{
-                    name: 'John',
-                    data: [0, 4, 3, 5, 4, 10, 20, 4, 3, 5, 4, 10, 20, 3, 4, 3]
-                }, {
-                    name: 'Jane',
-                    data: [0, 3, 4, 3, 3, 5, 4, 1, 3, 4, 3, 4, 5, 4, 1, 3]
-                }, {
-                    name: 'Man',
-                    data: [0, 7, 9, 9, 6, 5, 8, 6, 7, 9, 10, 6, 5, 4, 8, 9]
-                }]
+					name: 'Asia',
+					data: [502, 635, 809, 947, 1402, 3634, 5268]
+				}, {
+					name: 'Africa',
+					data: [106, 107, 111, 133, 221, 767, 1766]
+				}, {
+					name: 'Europe',
+					data: [163, 203, 276, 408, 547, 729, 628]
+				}, {
+					name: 'America',
+					data: [18, 31, 54, 156, 339, 818, 1201]
+				}, {
+					name: 'Oceania',
+					data: [2, 2, 2, 6, 13, 30, 46]
+				}]
             });
     });
        
